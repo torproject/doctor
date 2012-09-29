4ef39d487722db99d8889b837aabcb4408c11101
Karsten Loesing <karsten.loesing@gmx.net>
2012-09-29 10:54:57 -0400

    Faravahar is actually all lower-case.

--- src/org/torproject/doctor/Checker.java
+++ src/org/torproject/doctor/Checker.java
@@ -95,7 +95,7 @@ public class Checker {
   private void checkMissingConsensuses() {
     SortedSet<String> missingConsensuses = new TreeSet<String>(
         Arrays.asList(("gabelmoo,tor26,turtles,maatuska,dannenberg,urras,"
-        + "moria1,dizum,Faravahar").split(",")));
+        + "moria1,dizum,faravahar").split(",")));
     missingConsensuses.removeAll(this.downloadedConsensuses.keySet());
     if (!missingConsensuses.isEmpty()) {
       this.warnings.put(Warning.ConsensusDownloadTimeout,
@@ -377,7 +377,7 @@ public class Checker {
   private void checkMissingVotes() {
     SortedSet<String> knownAuthorities = new TreeSet<String>(
         Arrays.asList(("dannenberg,dizum,gabelmoo,turtles,maatuska,"
-        + "moria1,tor26,urras,Faravahar").split(",")));
+        + "moria1,tor26,urras,faravahar").split(",")));
     SortedSet<String> missingVotes =
         new TreeSet<String>(knownAuthorities);
     for (RelayNetworkStatusVote vote : this.downloadedVotes) {
@@ -416,7 +416,7 @@ public class Checker {
   private void checkMissingAuthorities() {
     SortedSet<String> missingAuthorities = new TreeSet<String>(
         Arrays.asList(("gabelmoo,tor26,turtles,maatuska,dannenberg,urras,"
-        + "moria1,dizum,Faravahar,Tonga").split(",")));
+        + "moria1,dizum,faravahar,Tonga").split(",")));
     for (NetworkStatusEntry entry :
         this.downloadedConsensus.getStatusEntries().values()) {
       if (entry.getFlags().contains("Authority")) {
