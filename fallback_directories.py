ce681b895fd6168047c010f47e4fc393eeaeb564
Damian Johnson <atagar@torproject.org>
2018-10-28 13:32:06 -0700

    Bump fallback notification theshold (25% => 50%)
    This has been notifying everyone for weeks due to breaching the 25% thresold.
    Four days ago I reached out to teor and Nick to see what we want to do with
    this but haven't heard back.
    
    DocTor notices need to spur action or they're just noise. Bumping the threshold
    to 50% so this no longer emails us.

--- fallback_directories.py
+++ fallback_directories.py
@@ -16,7 +16,7 @@ import util
 
 log = util.get_logger('fallback_directories')
 
-NOTIFICATION_THRESHOLD = 25  # send notice if this percentage of fallbacks are unusable
+NOTIFICATION_THRESHOLD = 50  # send notice if this percentage of fallbacks are unusable
 TO_ADDRESSES = ['tor-consensus-health@lists.torproject.org', 'teor@torproject.org', 'nickm@torproject.org']
 EMAIL_SUBJECT = 'Fallback Directory Summary (%i/%i, %i%%)'
 
